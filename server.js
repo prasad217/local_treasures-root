0d9ab5e3358a35b7473f615eae362f5a47f65df5
prasad217 <cr6473@srmist.edu.in>
2024-07-11 00:37:38 +0530

    updated bycrpt to bycryptjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const multer = require('multer');
 const cookieParser = require('cookie-parser');
